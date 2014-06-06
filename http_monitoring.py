6ab93280c061f2f6d5ce2ed44640f503c0259149
Ron Trompert <ron.trompert@surfsara.nl>
2014-06-07 00:00:41 +0200

    rearranging stuff

--- http_monitoring.py
+++ http_monitoring.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 
-PROPFIND | grep -v GET | grep -v POST | grep -v PUT | grep -v HEAD | grep -v PROPPATCH | grep -v MOVE | grep -v MKCOL
+#PROPFIND
+#GET
+#POST
+#PUT
+#HEAD
+#PROPPATCH
+#MOVE
+#MKCOL
+
 import sys
 import re
 from string import strip
+import mysql.connector
+
+db='db'
+dbuser='dbuser'
+dbhost='dbhost'
+dbpasswd='dbpasswd'
 
 match=re.compile('^(.+)\s+\-\s+(.+)\s+\[(.+)\]\s+\"(.+)\"\s+([0-9]+)\s+\-\s+\"(.+)\"\s+\"(.+)\"')
 match2=re.compile('^(.+)\s+\-\s+(.+)\s+\[(.+)\]\s+\"(.+)\"\s+([0-9]+)\s+([\-0-9]+)\s+\"(.+)\"\s+\"(.+)\"')
 
-def main ():
+def parse_file (file):
 
-    with open('/etc/httpd/logs/access_log','r') as f:
+    with open(file,'r') as f:
         while True:
             lines = f.readlines(8192)
             if not lines:
@@ -20,28 +34,32 @@ def main ():
                 if m2 != None:
                     list=m2.groups()
                     ip=list[0]
-                    eppn=list[1]
+                    user=list[1]
                     timestamp=list[2]
                     request=list[3]
                     code=list[4]
                     size=list[5]
-                    file=list[6]
-                    browser=list[7]
+                    referer=list[6]
+                    user_agent=list[7]
                 else:
                     m=match.match(strip(line))
                     if m != None:
                         list=m.groups()
                         ip=list[0]
-                        eppn=list[1]
+                        user=list[1]
                         timestamp=list[2]
                         request=list[3]
                         code=list[4]
                         size=0
-                        file=list[5]
-                        browser=list[6]
+                        referer=list[5]
+                        user_agent=list[6]
                     else:
                         print "Not able to parse "+line
                         sys.exit(1)
+
+def main():
+    file='/etc/httpd/logs/access_log'
+    parse_file(file)
 
 if __name__ == '__main__':
 
