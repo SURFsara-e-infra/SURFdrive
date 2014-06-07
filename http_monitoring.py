56af68db12e39357555343f895e2c6bd5bf67748
Ron Trompert <ron.trompert@surfsara.nl>
2014-06-07 17:55:47 +0200

    progress made

--- http_monitoring.py
+++ http_monitoring.py
@@ -9,18 +9,35 @@
 #MOVE
 #MKCOL
 
+import os
 import sys
 import re
 from string import strip
-import mysql.connector
+#import mysql.connector
+from os import listdir
+from os.path import isfile, join
 
 db='db'
 dbuser='dbuser'
 dbhost='dbhost'
 dbpasswd='dbpasswd'
+ 
+logdir='logs'
+parsed_files_file='/var/tmp/http_monitoring_parsed_files.txt'
 
 match=re.compile('^(.+)\s+\-\s+(.+)\s+\[(.+)\]\s+\"(.+)\"\s+([0-9]+)\s+\-\s+\"(.+)\"\s+\"(.+)\"')
 match2=re.compile('^(.+)\s+\-\s+(.+)\s+\[(.+)\]\s+\"(.+)\"\s+([0-9]+)\s+([\-0-9]+)\s+\"(.+)\"\s+\"(.+)\"')
+almatch=re.compile('^access\_log\-([0-9]{8})$')
+
+def get_access_logs(dir):
+    files = [ f for f in listdir(dir) if isfile(join(dir,f)) ]
+  
+    alfiles=[]
+    for file in files:
+        if almatch.match(file)!=None:
+            alfiles.append(file)
+
+    return alfiles
 
 def parse_file (file):
 
@@ -57,9 +74,59 @@ def parse_file (file):
                         print "Not able to parse "+line
                         sys.exit(1)
 
+def get_dates_from_alfile_names(alfiles):
+
+    dates=[]
+    for alf in alfiles:
+        dates.append(long(almatch.match(alf).groups()[0]))
+
+    return sorted(dates)
+
+def get_parsed_files_from_file():
+    if os.path.isfile(parsed_files_file):
+        f=open(parsed_files_file,'r')
+        pf=strip(f.readline()).split(';')
+        f.close()
+    else:
+        pf=[]
+
+    return pf
+
+def finalize_parsing(alfiles):
+    
+    for file in alfiles:
+        if alfiles.index(file) == 0:
+            txt=file
+        else:
+            txt=txt+';'+file
+    
+    f=open(parsed_files_file,'w')
+    f.write(txt)
+    f.close()
+
+    
+
+def get_files_to_parse(alfiles):
+
+    parsed_files=get_parsed_files_from_file()
+
+    files_to_parse=[]
+    for file in alfiles:
+        if file not in parsed_files: files_to_parse.append(file)
+
+    return files_to_parse
+    
+
+
 def main():
-    file='/etc/httpd/logs/access_log'
-    parse_file(file)
+
+    alfiles=get_access_logs(logdir)
+    files=get_files_to_parse(alfiles)
+    for file in files:
+#        parse_file(file)
+         print file
+    finalize_parsing(alfiles)
+
 
 if __name__ == '__main__':
 
